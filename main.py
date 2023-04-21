2610ae62d48e8e10ee6696490c6fe062d61a1552
Negev2333 <130639602+Negev2333@users.noreply.github.com>
2023-04-21 08:48:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ bot = commands.Bot(command_prefix="+")
 @bot.event
 async def on_ready():
     print(">> Online <<")
-    await bot.change_presence(activity=discord.Game(name="陰陽師 / +help查看更多"))
+    await bot.change_presence(activity=discord.Game(name="NIKKE / !help查看更多"))
     count = 0
     for _ in bot.guilds:
         count +=1
@@ -20,9 +20,6 @@ async def on_command_error(ctx, error):
         return
 
 bot.remove_command("help")
-bot.load_extension("cogs.About")
 bot.load_extension("cogs.Help")
 bot.load_extension("cogs.Roll")
-bot.load_extension("cogs.Search")
-bot.load_extension("cogs.Updates")
 bot.run(os.environ['TOKEN'])
